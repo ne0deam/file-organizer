7063c2d70a7b8e0b0a81adc006a1c3d86d192cb5
Eugene <ne092835@gmail.com>
2026-05-21 15:21:12 +0300

    add FileOrganizer struct constructor

--- main.go
+++ main.go
@@ -1,25 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var DefaultRules = map[string]string{
+	".jpg":  "Images",
+	".jpeg": "Images",
+	".png":  "Images",
+	".pdf":  "Documents",
+	".doc":  "Documents",
+	".docx": "Documents",
+	".txt":  "Documents",
+	".mp3":  "Music",
+	".wav":  "Music",
+	".mp4":  "Video",
+	".avi":  "Video",
+	".zip":  "Archives",
+	".rar":  "Archives",
+}
+
+type FileOrganizer struct {
+	sourceDir      string
+	rulesMap       map[string]string
+	processedFiles int
+	logFile        *os.File
+}
+
+func NewFileOrganizer(sourceDir string) (*FileOrganizer, error) {
+	if sourceDir == "" {
+		return nil, fmt.Errorf("sourceDir не может быть пустым")
+	}
+
+	fileInfo, err := os.Stat(sourceDir)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось получить информацию о %q: %w", sourceDir, err)
+	}
+	if !fileInfo.IsDir() {
+		return nil, fmt.Errorf("%q не является директорией", sourceDir)
+	}
+
+	return &FileOrganizer{
+		sourceDir: sourceDir,
+		rulesMap:  DefaultRules,
+	}, nil
+}
 
 func main() {
-	DefaultRules := map[string]string{
-		".jpg":  "Images",
-		".jpeg": "Images",
-		".png":  "Images",
-		".pdf":  "Documents",
-		".doc":  "Documents",
-		".docx": "Documents",
-		".txt":  "Documents",
-		".mp3":  "Music",
-		".wav":  "Music",
-		".mp4":  "Video",
-		".avi":  "Video",
-		".zip":  "Archives",
-		".rar":  "Archives",
+	organizer, err := NewFileOrganizer("/home/neo/stratus")
+	if err != nil {
+		fmt.Println("Ошибка:", err)
 	}
 
-	for key, value := range DefaultRules {
-		fmt.Println("Расширение:", key, "-> Папка:", value)
-	}
+	fmt.Println("FileOrganizer создан для директории:", organizer.sourceDir)
 }
